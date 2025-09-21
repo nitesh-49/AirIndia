533e1f7ce0233bb0721a862367d84372cd041a65
nitesh-49 <nitesh.solanki555@gmail.com>
2025-09-21 23:34:59 +0530

    Update FlightTImings.java
    add new var

--- AirIndia/src/main/java/FlightTImings.java
+++ AirIndia/src/main/java/FlightTImings.java
@@ -2,9 +2,11 @@
 public class FlightTImings {
 
 	public static void main(String[] args) {
+		int a=10;
 		System.out.print("we are int the cloud wooou ....!!!!");
 		System.out.println(System.currentLocalDateAndTime());
 	}
 
 }
+
 
