910f43b50c775b350727de2bd1dca6c33fb6efaa
nitesh-49 <nitesh.solanki555@gmail.com>
2025-09-21 23:28:47 +0530

    Update FlightTImings.java
    current localdate updated

--- AirIndia/src/main/java/FlightTImings.java
+++ AirIndia/src/main/java/FlightTImings.java
@@ -3,7 +3,8 @@ public class FlightTImings {
 
 	public static void main(String[] args) {
 		System.out.print("we are int the cloud wooou ....!!!!");
-
+		System.out.println(System.currentLocalDateAndTime());
 	}
 
 }
+
